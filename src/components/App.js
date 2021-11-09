3c934642d6b29d5d413e24d59add963145de9840
Artem Chumak <artem.4x@gmail.com>
2021-11-09 17:19:50 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,55 @@ import logo from "../images/logo.png"
 import { useState } from "react"
 
 function App() {
+
+  const [user, setUser] = useState({
+    email: "user@mail.ru",
+    password: "123456",
+  })
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  })
+  const [isUserWrong, setUserWrong] = useState(false)
+  const [isPasswordForgotten, setPasswordForgotten] = useState(false);
+  const [isPasswordSent, setPasswordSent] = useState(false)
+
+  const handlePassForgotten = () => {
+    isPasswordForgotten ? setPasswordForgotten(false) : setPasswordForgotten(true);
+  }
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData({
+      ...data,
+      [name]: value,
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { email, password } = data;
+    handleLogin(email, password);
+  }
+
+  const handleSubmitNewPass = (e) => {
+    e.preventDefault();
+    setPasswordForgotten(false);
+    setPasswordSent(true);
+    e.target.reset();
+  }
+
+  const handleLogin = (email, password) => {
+    if (email !== user.email || password !== user.password) {
+      setUserWrong(true);
+    }
+  }
+
+  const handleCloseAlert = () => {
+    setUserWrong(false);
+    setPasswordSent(false);
+  }
+
   return (
     <>
       <header className="header">
@@ -9,30 +58,30 @@ function App() {
       </header>
       <main className="main">
         <h1 className="title">Sign in to App</h1>
-        <div className="redallert">
+        <div className={isUserWrong ? "redallert" : "hide"}>
           <p className="allert_text">Incorrect username or password.</p>
-          <button className="allert_exit"></button>
+          <button onClick={handleCloseAlert} className="allert_exit"></button>
         </div>
         <div className="signin_box">
-          <form className="signin_form" action="#" name="signin">
+          <form onSubmit={handleSubmit} className="signin_form" action="#" name="signin">
             <label className="lable" forhtml="email">Email address</label>
-            <input className="input" type="email" name="email" id="email" required />
+            <input value={data.email} onChange={handleChange} className="input" type="email" name="email" id="email" required />
             <div className="lable_box">
               <label className="lable" forhtml="password">Password</label>
-              <span className="link">Forgot password?</span>
+              <span onClick={handlePassForgotten} className="link">Forgot password?</span>
             </div>
-            <input className="input" type="password" name="password" id="password" required />
+            <input value={data.password} onChange={handleChange} className="input" type="password" name="password" id="password" required />
             <button className="signin_btn" type="submit" aria-label="Sign in">Sign in</button>
           </form>
         </div>
 
-        <div className="greenallert">
+        <div className={isPasswordSent ? 'greenallert' : 'hide'}>
           <p className="allert_text">New password was sent</p>
-          <button className="greenallert_exit"></button>
+          <button onClick={handleCloseAlert} className="greenallert_exit"></button>
         </div>
 
-        <div className="forgot_box">
-          <form className="forgot_form" action="#" name="forgot">
+        <div className={isPasswordForgotten ? "forgot_box" : "hide"}>
+          <form onSubmit={handleSubmitNewPass} className="forgot_form" action="#" name="forgot">
             <label className="lable" forhtml="forgot_email">Email address</label>
             <input className="forgot_input" type="email" name="forgot_email" id="forgot_email" required />
             <button className="signin_btn" type="submit" aria-label="Send new password">Send new password</button>
